badef115c5b92ca111b0d6037e3d6e17f39007a5
Ondra Mandik <mandik@spsejecna.cz>
2017-01-06 16:29:35 +0100

    test

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,15 @@
 public class Main {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		//todo everything
 
 		System.out.println("Jana");
 		System.out.println("Tonda");
-		System.out.println("Šárka");
+		System.out.println("ï¿½ï¿½rka");
 
 
 
 		System.out.println("Masopust");
-=======
-		//SMAZANO vse
->>>>>>> branch 'master' of https://github.com/ondramandik/vzorova-maturitni-prace.git
 
 	}
 

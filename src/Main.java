b06022c501fe63d6d9a7a021672eb984839b83fc
HeleTy <spilkova@spsejecna.cz>
2017-01-06 16:30:17 +0100

    znovu

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,8 @@
 public class Main {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-=======
-		//todo everything
->>>>>>> branch 'master' of https://github.com/ondramandik/vzorova-maturitni-prace.git
 
-<<<<<<< HEAD
 		System.out.println("Vy paka");
-=======
-		System.out.println("Jana");
-		System.out.println("Tonda");
-		System.out.println("��rka");
-
-
-
-		System.out.println("Masopust");
->>>>>>> branch 'master' of https://github.com/ondramandik/vzorova-maturitni-prace.git
 
 	}
 

d70a3ad965d99a8437adce9729c54ac5228b3d4b
alik <alik@alenka>
2017-01-20 20:22:48 +0100

    Zmena name component

--- src/okna/NovyZakaznik.java
+++ src/okna/NovyZakaznik.java
@@ -13,21 +13,21 @@ import javax.swing.JButton;
 public class NovyZakaznik extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
-	private JTextField textField_6;
-	private JTextField textField_7;
-	private JTextField textField_8;
+	private JTextField jmenoField;
+	private JTextField prijmeniField;
+	private JTextField uliceField;
+	private JTextField popisneCisloField;
+	private JTextField orientacniCisloField;
+	private JTextField mestoField;
+	private JTextField pscField;
+	private JTextField telefonField;
+	private JTextField mailField;
 
 	/**
 	 * Create the frame.
 	 */
 	public NovyZakaznik() {
-		setTitle("Nový zákazník");
+		setTitle("Zákazník");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 581, 573);
 		contentPane = new JPanel();
@@ -35,68 +35,68 @@ public class NovyZakaznik extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblJmno = new JLabel("Jméno:");
-		lblJmno.setBounds(26, 37, 56, 16);
-		contentPane.add(lblJmno);
+		JLabel lblJmeno = new JLabel("Jméno:");
+		lblJmeno.setBounds(26, 37, 56, 16);
+		contentPane.add(lblJmeno);
 		
-		JLabel lblPjmen = new JLabel("Příjmení:");
-		lblPjmen.setBounds(26, 78, 56, 16);
-		contentPane.add(lblPjmen);
+		JLabel lblPrijmeni = new JLabel("Příjmení:");
+		lblPrijmeni.setBounds(26, 78, 56, 16);
+		contentPane.add(lblPrijmeni);
 		
-		textField = new JTextField();
-		textField.setBounds(154, 34, 116, 22);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		jmenoField = new JTextField();
+		jmenoField.setBounds(154, 34, 116, 22);
+		contentPane.add(jmenoField);
+		jmenoField.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(154, 75, 116, 22);
-		contentPane.add(textField_1);
-		textField_1.setColumns(10);
+		prijmeniField = new JTextField();
+		prijmeniField.setBounds(154, 75, 116, 22);
+		contentPane.add(prijmeniField);
+		prijmeniField.setColumns(10);
 		
 		JLabel lblUlice = new JLabel("Název ulice:");
 		lblUlice.setBounds(26, 123, 79, 16);
 		contentPane.add(lblUlice);
 		
-		JLabel lblNewLabel = new JLabel("Číslo popisné:");
-		lblNewLabel.setBounds(26, 165, 90, 16);
-		contentPane.add(lblNewLabel);
+		JLabel lblCisloPopisne = new JLabel("Číslo popisné:");
+		lblCisloPopisne.setBounds(26, 165, 90, 16);
+		contentPane.add(lblCisloPopisne);
 		
-		JLabel lblsloOrientan = new JLabel("Číslo orientační:");
-		lblsloOrientan.setBounds(26, 212, 107, 16);
-		contentPane.add(lblsloOrientan);
+		JLabel lblCisloOrientan = new JLabel("Číslo orientační:");
+		lblCisloOrientan.setBounds(26, 212, 107, 16);
+		contentPane.add(lblCisloOrientan);
 		
-		textField_2 = new JTextField();
-		textField_2.setBounds(154, 120, 116, 22);
-		contentPane.add(textField_2);
-		textField_2.setColumns(10);
+		uliceField = new JTextField();
+		uliceField.setBounds(154, 120, 116, 22);
+		contentPane.add(uliceField);
+		uliceField.setColumns(10);
 		
-		textField_3 = new JTextField();
-		textField_3.setBounds(154, 162, 116, 22);
-		contentPane.add(textField_3);
-		textField_3.setColumns(10);
+		popisneCisloField = new JTextField();
+		popisneCisloField.setBounds(154, 162, 116, 22);
+		contentPane.add(popisneCisloField);
+		popisneCisloField.setColumns(10);
 		
-		textField_4 = new JTextField();
-		textField_4.setBounds(154, 209, 116, 22);
-		contentPane.add(textField_4);
-		textField_4.setColumns(10);
+		orientacniCisloField = new JTextField();
+		orientacniCisloField.setBounds(154, 209, 116, 22);
+		contentPane.add(orientacniCisloField);
+		orientacniCisloField.setColumns(10);
 		
-		JLabel lblNewLabel_1 = new JLabel("Město: ");
-		lblNewLabel_1.setBounds(26, 257, 56, 16);
-		contentPane.add(lblNewLabel_1);
+		JLabel lblMesto = new JLabel("Město: ");
+		lblMesto.setBounds(26, 257, 56, 16);
+		contentPane.add(lblMesto);
 		
-		JLabel lblNewLabel_2 = new JLabel("PSČ:");
-		lblNewLabel_2.setBounds(26, 295, 56, 16);
-		contentPane.add(lblNewLabel_2);
+		JLabel lblPSC = new JLabel("PSČ:");
+		lblPSC.setBounds(26, 295, 56, 16);
+		contentPane.add(lblPSC);
 		
-		textField_5 = new JTextField();
-		textField_5.setBounds(154, 244, 116, 22);
-		contentPane.add(textField_5);
-		textField_5.setColumns(10);
+		mestoField = new JTextField();
+		mestoField.setBounds(154, 244, 116, 22);
+		contentPane.add(mestoField);
+		mestoField.setColumns(10);
 		
-		textField_6 = new JTextField();
-		textField_6.setBounds(154, 292, 116, 22);
-		contentPane.add(textField_6);
-		textField_6.setColumns(10);
+		pscField = new JTextField();
+		pscField.setBounds(154, 292, 116, 22);
+		contentPane.add(pscField);
+		pscField.setColumns(10);
 		
 		JLabel lblTelefon = new JLabel("Telefon:");
 		lblTelefon.setBounds(26, 340, 56, 16);
@@ -106,17 +106,17 @@ public class NovyZakaznik extends JFrame {
 		lblEmail.setBounds(26, 385, 56, 16);
 		contentPane.add(lblEmail);
 		
-		textField_7 = new JTextField();
-		textField_7.setBounds(154, 337, 116, 22);
-		contentPane.add(textField_7);
-		textField_7.setColumns(10);
+		telefonField = new JTextField();
+		telefonField.setBounds(154, 337, 116, 22);
+		contentPane.add(telefonField);
+		telefonField.setColumns(10);
 		
-		textField_8 = new JTextField();
-		textField_8.setBounds(154, 379, 116, 22);
-		contentPane.add(textField_8);
-		textField_8.setColumns(10);
+		mailField = new JTextField();
+		mailField.setBounds(154, 379, 116, 22);
+		contentPane.add(mailField);
+		mailField.setColumns(10);
 		
-		JButton btnUloit = new JButton("Uložení");
+		JButton btnUloit = new JButton("Uložit");
 		btnUloit.setBounds(26, 459, 97, 25);
 		contentPane.add(btnUloit);
 		

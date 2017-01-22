414778449f18724faf74eff46bffe158be634298
Jarda Jagr <jarda@seznam.cz>
2017-01-22 20:03:46 +0100

    Test

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,10 @@
-import java.io.IOException;
-import java.sql.SQLException;
-
 import databaze.*;
 import entity.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-		//test1();
+		test1();
 		test2();
 	}
 	
